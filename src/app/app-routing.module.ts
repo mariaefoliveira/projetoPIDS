52edab314069e8ea696abe4d9880be3938f1633e
noragan03 <dudafalcaodeoliveira@gmail.com>
2021-09-05 17:50:50 -0300

    template finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { PedidoFormComponent } from './paginas/pedido-form/pedido-form.component';
 import { CardapioPedidoComponent } from './paginas/cardapio-pedido/cardapio-pedido.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', component: CardapioPedidoComponent}
+  {path: '', component: CardapioPedidoComponent},
+  {path: 'fazer-pedido', component: PedidoFormComponent},
 ];
 
 @NgModule({
